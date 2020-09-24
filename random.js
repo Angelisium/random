40c0db5fbef4ad7c6cadad570dba08eaaae50db0
アレチ <eliard.alexis@gmail.com>
2020-09-24 14:00:16 +0200

    Update random.js

--- random.js
+++ random.js
@@ -29,18 +29,18 @@
 			return me.s2 = t - (me.c = t | 0);
 		};
 
-  		// Apply the seeding algorithm from Baagoe.
-		  me.c = 1;
-		  me.s0 = mash(' ');
-		  me.s1 = mash(' ');
-		  me.s2 = mash(' ');
-		  me.s0 -= mash(seed);
-		  if (me.s0 < 0) { me.s0 += 1; }
-		  me.s1 -= mash(seed);
-		  if (me.s1 < 0) { me.s1 += 1; }
-		  me.s2 -= mash(seed);
-		  if (me.s2 < 0) { me.s2 += 1; }
-		  mash = null;
+		// Apply the seeding algorithm from Baagoe.
+		me.c = 1;
+		me.s0 = mash(' ');
+		me.s1 = mash(' ');
+		me.s2 = mash(' ');
+		me.s0 -= mash(seed);
+		if (me.s0 < 0) { me.s0 += 1; }
+		me.s1 -= mash(seed);
+		if (me.s1 < 0) { me.s1 += 1; }
+		me.s2 -= mash(seed);
+		if (me.s2 < 0) { me.s2 += 1; }
+		mash = null;
 	}
 
 	function copy(f, t) {
